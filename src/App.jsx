d6a241cddb1cd9505966023e1546c600ecbfa8f2
evyy2008 <rm90296@estudante.fieb.edu.br>
2025-03-19 08:27:53 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,34 @@ import { useState } from "react"
 function App(){
  
   const [nome, setNome] = useState('Ronaldo')
+
+  const Formulario = () => {
+
+    return(
+      <div>
+        <input
+        className="nome"
+        onChange={(e)=>{
+          setNome(e.target.value)}}
+        name="nome"
+        type="text" />
+      <button
+        onClick={()=>{alert(nome);}}
+        className="botao">
+        CLIQUE AQUI
+      </button>
+      </div>
+    )
+  }
  
   return (
     <div>
       <h3>Pizzaria 2C</h3>
-      <input
-        className="nome"
-        onChange={(e)=>{setNome(e.target.value)}}
-        name="nome"
-        type="text" />
-      <button
-        onClick={()=>{alert(nome)}}
-        className="botao">
-        CLIQUE AQUI
-      </button>
+      <Formulario/>
+      <Formulario/>
+      <Formulario/>
     </div>
-  )
+  );
 }
  
-export default App
+export default App;
